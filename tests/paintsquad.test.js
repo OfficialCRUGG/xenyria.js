782d950ccd8b6cbb18f1de877b15c1a7825738bf
OfficialCRUGG <ich@crugg.de>
2023-05-05 20:02:14 +0200

    chore: remove console.log from test

--- tests/paintsquad.test.js
+++ tests/paintsquad.test.js
@@ -6,7 +6,6 @@ const xenyria = new Xenyria(process.env.XENYRIA_TOKEN);
 
 test(".paintsquad.versionData()", async () => {
   const response = await xenyria.paintsquad.versionData();
-  console.log(response.data);
   expect(Object.values(response.data?.modes).length > 0).toEqual(true);
   expect(response.success).toEqual(true);
 });
